e86c2ffb8671661959a9235be2a760db66b42923
Joyce <jujoyce12913804@gmail.com>
2021-12-08 04:28:47 -0600

    styled login page

--- client/src/components/login.tsx
+++ client/src/components/login.tsx
@@ -1,8 +1,37 @@
 import React, { useEffect } from "react";
 import "./login.css";
+import bg from "../assests/quad.jpg";
+import logo from "../assests/uiuc logo.png"
 
 const Login = () => {
-  return <h1>login</h1>;
+  return (
+    <div id="background">
+      <img src={bg} className="fit" alt=""/>
+      <div id="logo">
+        <img src={logo} className="fit2" alt=""/>
+        <p className="imgtxt">ON-CAMPUS JOB BOARD</p>
+      </div>
+      <div className="leftCard">
+        <div className="welcome">
+          <p id="welcome">WELCOME BACK</p>
+          <p>Please enter your email and password</p>
+        </div>
+        <div className="formcont">
+          <form>
+            <p className="label">EMAIL:</p>
+              <input type="text" className="inputbox"/>
+            <p className="label">PASSWORD:</p>
+              <input type="text" className="inputbox"/>
+            <button type="submit" className="loginbtn">
+              Log In
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+    
+  
+    );
 };
 
 export default Login;
