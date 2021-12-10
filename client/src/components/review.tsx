af7234360b55973826f04239a662f405ce30aaf9
Joyce <jujoyce12913804@gmail.com>
2021-12-10 17:20:06 -0600

    test rating

--- client/src/components/review.tsx
+++ client/src/components/review.tsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import bg from "../assests/quad.jpg";
 import "./review.css";
 
 const Review = () => {
-  return (
-    <div id="background">
-        <img src={bg} className="fit" alt=""/>
-        <div className="card">
+  let Rating = require("react-rating");
+  React.createElement(Rating);
+  const [rating, setRating] = useState(0);
 
+  const handleClick = (rate: number) => {
+    setRating(rate);
+  };
+
+  return (
+    <div id="bg">
+      <img src={bg} className="fitimg" alt="" />
+      <div className="card">
+        <div className="avgrate">
+          <Rating
+            start={0}
+            stop={5}
+            step={1}
+            fractions={1}
+            initialRaing={rating}
+            direction="ltr"
+            onClick={handleClick}
+          />
         </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Review;
