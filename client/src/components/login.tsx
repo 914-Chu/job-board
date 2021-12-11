46916d93dafdfa34445ba4fcc6030152ea4e2d28
Joyce <jujoyce12913804@gmail.com>
2021-12-11 12:16:39 -0600

    update login page

--- client/src/components/login.tsx
+++ client/src/components/login.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./login.css";
 import logo from "../assests/uiuc logo.png"
 import { Link } from "react-router-dom";
+import { Card, Form, Button, Col, Row } from "react-bootstrap";
 
 const Login = () => {
   document.body.style.overflow = "hidden";
@@ -11,13 +12,28 @@ const Login = () => {
         <img src={logo} className="fit2" alt="" />
         <p className="imgtxt">ON-CAMPUS JOB BOARD</p>
       </div>
-      <div className="containleft">      </div>
+      <div className="containleft"></div>
         <div className="leftCard">
           <div className="welcome">
             <p id="welcome">WELCOME BACK</p>
             <p>Please enter your email and password</p>
           </div>
-          <div className="formcont">
+          <Form className="login-form">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Link to="/main">
+              <Button type="submit" id="loginbtn">
+                Submit
+              </Button>
+            </Link>
+          </Form>
+          {/* <div className="formcont">
             <form>
               <p className="label">EMAIL:</p>
               <input type="text" className="inputbox" />
@@ -29,7 +45,7 @@ const Login = () => {
                 </button>
               </Link>
             </form>
-          </div>
+          </div> */}
         </div>
     </div>
   );
