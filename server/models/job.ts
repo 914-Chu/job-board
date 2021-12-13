8ae8d9db6447092404241526d5ef07e5b33a146e
AlyxJM <merritt9@illinois.edu>
2021-12-13 16:19:56 -0600

    mongo url fix, add fields, get & post jobs

--- server/models/job.ts
+++ server/models/job.ts
@@ -1,16 +1,18 @@
-// Load required packages
 import mongoose from 'mongoose';
 
-// Define our task schema
 const JobSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
+    externalLink: { type: String, required: true},
     location: { type: String, required: true },
     hourlyPay: { type: Number, required: true },
     employmentType: { type: String, required: true},
     weeklyHours: { type: Number, requried: true },
-    dateCreated: {type: Date, required: true, default: Date.now}
+    tags: { type: [String], default: [] },
+    relatedMajors: { type: [String], default: [] },
+    averageRating: { type: Number, default: 0 },
+    postAuthor: { type: String, default: "" },
+    datePosted: {type: Date, required: true, default: Date.now}
 });
 
-// Export the Mongoose model
 export default mongoose.model('job', JobSchema, 'jobs');
