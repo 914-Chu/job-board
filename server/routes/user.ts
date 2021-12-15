53208119efb002fd5ebf710d2f3c1173dc773c5c
AlyxJM <merritt9@illinois.edu>
2021-12-15 15:53:50 -0600

    remove unused delete endpt, find by email vs id

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -2,33 +2,16 @@ import User from '../models/user';
 
 export default function (router) {
 
-    const userRoute = router.route('/users/:id');
+    const userRoute = router.route('/users/:email');
 
     userRoute.get(async (req, res) => {
-        User.findById(req.params.id, function (err, user) {
-            if (!user) {
+        User.find({email:req.params.email}, function (err, resData) {
+            if (resData.length === 0) {
                 res.status(404);
-                res.json({message: "User with provided id does not exist", data: req.params.id });
+                res.json({message: "User with provided email does not exist", data: req.params.email });
             } else {
                 res.status(200);
-                res.json({ message: 'OK', data: user });
-            }
-        });
-    });
-
-    userRoute.delete( async (req, res) => {
-        User.findByIdAndDelete(req.params.id, function (err, user) {
-            if (!user) {
-                res.status(404);
-                res.json({message: "User with provided id does not exist", data: req.params.id });
-            } else {
-                if (err) {
-                    res.status(500);
-                    res.json({message: "Unable to delete user with provided id", data: req.params.id });
-                } else {
-                    res.status(200);
-                    res.json({ message: 'User deleted', data: req.params.id });
-                }
+                res.json({ message: 'OK', data: resData });
             }
         });
     });
